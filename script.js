19faf39245da4c736b273e3221773025400efe82
shox <angleeee6644@gmail.com>
2022-07-09 15:48:24 +0500

    js

---
+++ script.js
@@ -0,0 +1,42 @@
+
+var name = prompt('Введите своё имя')
+
+console.log(name);
+
+var age = prompt('Введите свой возраст')
+
+console.log(age);
+
+var question = +prompt('Решите примеры: Пример-1 (39 + 1 = ?)')
+
+console.log(('Ваш ответ = ') + question);
+
+console.log('Правильный ответ = ' + ( 39 + 1));
+
+var question = +prompt('Пример-2 (108 - 9 = ?)')
+
+console.log(('Ваш ответ = ') + question);
+
+console.log('Правильный ответ = ' + ( 108 - 9));
+
+var question = +prompt('Пример-3 (43 * 2 = ?)')
+
+console.log(('Ваш ответ = ') + question);
+
+console.log('Правильный ответ = ' + ( 43 * 2));
+
+var question = +prompt('Пример-4 (77 / 7 = ?)')
+
+console.log(('Ваш ответ = ') + question);
+
+console.log('Правильный ответ = ' + ( 77 / 7));
+
+var question = +prompt('Пример-5 (93 / 6 = ?)')
+
+console.log(('Ваш ответ = ') + question);
+
+console.log('Правильный ответ = ' + ( 93 % 6));
+
+alert('Перейдите в консоль разработчика для проверки решений примеров.')
+
+
